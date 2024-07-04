46560b4765bd00e664e52773bf32bc990c5ff6a8
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-07-04 20:03:37 +0530

    Update pin.py

--- pin.py
+++ pin.py
@@ -26,11 +26,41 @@ DATABASE_NAME = 'Pinterest_bot'
 COLLECTION_NAME = 'users'
 
 app = Client("pinterest_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-executor = ThreadPoolExecutor(max_workers=150)  
+executor = ThreadPoolExecutor(max_workers=160)  
 client = MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
 users_collection = db[COLLECTION_NAME]
+privacy_responses = {
+    "info_collect": "We collect the following user data:\n- First Name\n- Last Name\n- Username\n- User ID\n This are public telegram details that everyone can see.",
+    "why_collect": "The collected data is used solely for improving your experience with the bot and for processing the bot stats and to avoid spammers.",
+    "what_we_do": "We use the data to personalize your experience and provide better services.",
+    "what_we_do_not_do": "We do not share your data with any third parties.",
+    "right_to_process": "You have the right to access, correct, or delete your data. [Contact us](t.me/drxew) for any privacy-related inquiries."
+}
 
+@app.on_message(filters.command("privacy"))
+async def privacy_command(client, message):
+    privacy_button = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("Privacy Policy", callback_data="privacy_policy")]]
+    )
+    await message.reply_text("Select one of the below options for more information about how the bot handles your privacy.", reply_markup=privacy_button)
+
+@app.on_callback_query()
+async def handle_callback_query(client, callback_query: CallbackQuery):
+    data = callback_query.data
+    if data == "privacy_policy":
+        buttons = [
+            [InlineKeyboardButton("What Information We Collect", callback_data="info_collect")],
+            [InlineKeyboardButton("Why We Collect", callback_data="why_collect")],
+            [InlineKeyboardButton("What We Do", callback_data="what_we_do")],
+            [InlineKeyboardButton("What We Do Not Do", callback_data="what_we_do_not_do")],
+            [InlineKeyboardButton("Right to Process", callback_data="right_to_process")]
+        ]
+        await callback_query.message.edit_text("Our contact details \n Name: PinterestVideoDlBot \n Telegram: https://t.me/CodecArchive \n The bot has been made to protect and preserve privacy as best as possible. \n  Our privacy policy may change from time to time. If we make any material changes to our policies, we will place a prominent notice on https://t.me/CodecBots.", reply_markup=InlineKeyboardMarkup(buttons))
+    elif data in privacy_responses:
+        back_button = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Back", callback_data="privacy_policy")]]
+        )
 @app.on_message(filters.command("start") & filters.private)
 async def handle_start_command(client, message):
     
